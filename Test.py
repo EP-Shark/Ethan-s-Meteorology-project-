f7683cc3728c2077c3d9a34acdbce461a6c5f4ef
EP-Shark <e.dub.palmer@gmail.com>
2025-02-08 16:08:10 +0000

    Initial commit

---
+++ Test.py
@@ -0,0 +1,9 @@
+x = int(input('Please enter a value for x: '))
+if x < 0:
+    print ("Really a negative value?")
+elif x == 0:
+    print ("give me a little more than 0 dawg")
+elif x < 10:
+    print(x , "that number is pretty cool but you can do better")
+else:
+    print ("WHOOOOOHOOOOOO the number,", x, "is absolutely perfect")
